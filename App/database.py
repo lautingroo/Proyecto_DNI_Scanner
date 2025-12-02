e55b12da2fc7f1f07b3856e633b09cd72aff67fe
lautingroo <lautaroavicolli@gmail.com>
2025-12-02 15:25:55 -0300

    Add files via upload

---
+++ App/database.py
@@ -0,0 +1,12 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mariadb+mariadbconnector://root:1234@localhost:3306/mi_basedatos"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
